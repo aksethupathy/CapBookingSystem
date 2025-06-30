836e1ff4df105506294ddd8282bf9f143cbdf75b
dev037 <dev037@tp037>
2025-06-30 13:54:25 +0530

    Customer Registor

--- CapBooking/src/main/java/CabBookingRepository/CabBookingCustomerRepository.java
+++ CapBooking/src/main/java/CabBookingRepository/CabBookingCustomerRepository.java
@@ -1,12 +1,14 @@
 package CabBookingRepository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import CabBookingModel.Customer;
-import CabBookingService.Optinol;
+
 
 public interface CabBookingCustomerRepository extends JpaRepository<Customer, Long > {
 
-	Optinol<Customer> findByEmail();
+	Optional<Customer> findByEmail(String email);
 
 }
