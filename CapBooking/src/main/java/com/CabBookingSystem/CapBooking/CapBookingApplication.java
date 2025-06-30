55143c5139536f02e4ada691f795ff744a965bb8
dev037 <dev037@tp037>
2025-06-30 09:56:04 +0530

    Welcome Cab booking Project

---
+++ CapBooking/src/main/java/com/CabBookingSystem/CapBooking/CapBookingApplication.java
@@ -0,0 +1,13 @@
+package com.CabBookingSystem.CapBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapBookingApplication.class, args);
+	}
+
+}
---
+++ CapBooking/src/test/java/com/CabBookingSystem/CapBooking/CapBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CabBookingSystem.CapBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
